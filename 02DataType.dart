a7d529966cf96d7760f3926544e22254b3ac48cd
sankalpkushwaha <sankalprsm@gmail.com>
2022-05-21 02:40:54 +0545

    learn datatype

---
+++ 02DataType.dart
@@ -0,0 +1,25 @@
+void main(){
+/*String name ="sankalp kushwaha"; 
+String address ="kathmandu"; 
+int age =22;
+double salary=50000.00;
+bool isMarried =false;
+
+ print("my name is ${name}. I am from ${address}. My age is ${age}. My salary is ${salary}. I am married =${isMarried}");
+*/
+//DateTime dob = DateTime(1999,11,26);
+//print("my DOB is ${dob}");
+//print(DateTime.now()); 
+/*var currDt = DateTime.now();
+print(currDt.year); // 4
+print(currDt.weekday); // 4
+print(currDt.month); // 4
+print(currDt.day); // 2
+print(currDt.hour); // 15
+print(currDt.minute); // 21
+print(currDt.second); // 49-
+*/
+List<String> furniture =['bed', 'chair','rack'];
+List<int> price =[10000,5000,6000];
+print(furniture.length);
+}
