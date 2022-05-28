787ba0cb6dc1ae6cb49fd9afc4060222050aa77c
SANKALP <sankalprsm@gmail.com>
2022-05-28 22:33:50 +0545

    learn list

--- 03List.dart
+++ 03List.dart
@@ -1,5 +1,7 @@
 void main(){
   List<String> programmingLanguage =['c', 'c++','java','Dart'];
 List<int> price =[100,200,500,1000];
-  print(programmingLanguage[3]); 
+  print(programmingLanguage.length); 
+  programmingLanguage.add("Golang");
+
 }
