1ddebbf5be0ef3892237c3e677a7fec79d84dc13
sankalpkushwaha <sankalprsm@gmail.com>
2022-05-28 22:09:37 +0545

    learn list

--- 03List.dart
+++ 03List.dart
@@ -1,5 +1,5 @@
 void main(){
   List<String> programmingLanguage =['c', 'c++','java','Dart'];
 List<int> price =[100,200,500,1000];
-  print(programmingLanguage[1]); 
+  print(programmingLanguage[3]); 
 }
